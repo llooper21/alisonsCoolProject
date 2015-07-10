400c5fd71a284ce4677666c798a8e5dd9758bcd5
Jay Won <jason@sudokrew.com>
2015-07-09 21:06:17 -1000

    Updated with working demo for both built in audio controls and tweaks to the method they were trying for using DOM API for audio controls.

--- MusicApp.js
+++ MusicApp.js
@@ -1,5 +1,8 @@
+//if going this route, need to setup control buttons for pause also
+//check out API options http://www.w3schools.com/tags/ref_av_dom.asp
 $('#deathcab').click(function(){
   alert('test');
   $('#dcsound').get(0).play();
 });
+
  
