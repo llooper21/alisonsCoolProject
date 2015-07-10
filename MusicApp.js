130c6eef727cc7e9ae9a878094af7e9a22e47511
ShazamDude <student10@js-c02mxpzxg083.punahou.edu>
2015-07-10 13:17:46 -1000

    save

--- MusicApp.js
+++ MusicApp.js
@@ -1,8 +1,9 @@
 //if going this route, need to setup control buttons for pause also
 //check out API options http://www.w3schools.com/tags/ref_av_dom.asp
 $('#deathcab').click(function(){
-  alert('test');
   $('#dcsound').get(0).play();
 });
 
- 
+$('#goodgirl').click(function(){
+  $('#ggsound').get(0).play();
+});
